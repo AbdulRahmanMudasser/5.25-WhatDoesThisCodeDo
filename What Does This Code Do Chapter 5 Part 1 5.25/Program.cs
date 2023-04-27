c1886bde6cef67cff4d7ce1e9db719cd3a307cd6
Abdul Rahman <abdulrahmanmuddasser@gmail.com>
2023-04-27 13:26:23 +0500

    solution done

--- What Does This Code Do Chapter 5 Part 1 5.25/Program.cs
+++ What Does This Code Do Chapter 5 Part 1 5.25/Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿class C5Q25
+{
+    static void Main(string[] args)
+    {
+        int count = 1;
+
+        while (count <= 10)
+        {
+            Console.WriteLine(count % 2 == 1? "****" : "++++++++");
+
+            count++;
+        }
+    }
+}
